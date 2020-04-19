aea8f675db9e40becda2b3d17b7aaf2554adc2cf
Paulo Amosse Cuambe <37635729+paulocuambe@users.noreply.github.com>
2020-04-19 19:40:19 +0200

    adding polymorphic relationship between User and Patient models
    migration files for creating users and patients tables;

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,16 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('fullname');
+            $table->string('bi', 11)->nullable();
+            $table->date('birthdate')->nullable();
+            $table->enum('gender', ['male', 'female'])->default('male');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('address')->nullable();
+            $table->string('profile_type')->nullable();
+            $table->unsignedInteger('profile_id')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
