72f71da07e6645b8ef80d8483057702bbac929ec
Paulo Amosse Cuambe <37635729+paulocuambe@users.noreply.github.com>
2020-04-19 21:21:30 +0200

    added Contact model and its relantionship with the User model
    added a one to many relationship between the Users(one) and Contacts(many) models

--- app/Models/User.php
+++ app/Models/User.php
@@ -35,7 +35,13 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-    public function profile() {
+    public function profile()
+    {
         return $this->morphTo();
+    }
+
+    public function contacts()
+    {
+        return $this->hasMany(Contact::class);
     }
 }
