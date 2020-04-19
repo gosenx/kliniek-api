a62f9e9b14a957e9e1da1e401f05c817197fe3f7
Paulo Amosse Cuambe <37635729+paulocuambe@users.noreply.github.com>
2020-04-19 20:01:53 +0200

    adding patient_code field to patients table

--- database/migrations/2020_04_19_151841_create_patients_table.php
+++ database/migrations/2020_04_19_151841_create_patients_table.php
@@ -15,6 +15,7 @@ class CreatePatientsTable extends Migration
     {
         Schema::create('patients', function (Blueprint $table) {
             $table->id();
+            $table->unsignedInteger('patient_code')->nullable();
             $table->string('job_title')->nullable();
             $table->timestamps();
         });
