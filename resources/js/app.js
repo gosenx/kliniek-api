772e024ef3c6e034b267d08e6d1efd0dd88e6a6c
Paulo Amosse Cuambe <37635729+paulocuambe@users.noreply.github.com>
2020-04-22 14:24:33 +0200

    adding passport vue components

--- resources/js/app.js
+++ resources/js/app.js
@@ -18,8 +18,14 @@ window.Vue = require('vue');
 
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
+import Clients from "./components/passport/Clients";
+import AuthorizedClients from "./components/passport/AuthorizedClients";
+import PersonalAccessTokens from "./components/passport/PersonalAccessTokens";
 
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component('passport-clients', Clients);
+Vue.component('passport-authorized-clients', AuthorizedClients);
+Vue.component('passport-personal-access-tokens', PersonalAccessTokens);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
