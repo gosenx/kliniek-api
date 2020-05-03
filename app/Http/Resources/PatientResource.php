2f8f0e9f00934c2ff21b40e442824bfe3d4e7b83
Paulo Amosse Cuambe <37635729+paulocuambe@users.noreply.github.com>
2020-05-03 20:12:10 +0200

    adding PatientResource to render Patient specific data

---
+++ app/Http/Resources/PatientResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PatientResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id'=>$this->id,
+            'patient_code'=>$this->patient_code,
+            'job_title'=>$this->job_title,
+            'created_at'=>$this->created_at,
+            'updated_at'=>$this->created_at,
+        ];
+    }
+}
