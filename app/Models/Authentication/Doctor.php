11a94828e9f1b2dd44a023dff15b3fc9d4f5f740
Paulo Amosse Cuambe <37635729+paulocuambe@users.noreply.github.com>
2020-05-04 11:53:42 +0200

    adding relationship with appointments on Doctor model and methods to retrieve appointments in specific states

--- app/Models/Authentication/Doctor.php
+++ app/Models/Authentication/Doctor.php
@@ -2,6 +2,7 @@
 
 namespace App\Models\Authentication;
 
+use App\Models\Business\Appointment;
 use App\Models\Specialty;
 use Illuminate\Database\Eloquent\Model;
 
@@ -18,5 +19,25 @@ class Doctor extends Model
     public function specialty()
     {
         return $this->belongsTo(Specialty::class);
+    }
+
+    public function appointments()
+    {
+        return $this->hasMany(Appointment::class);
+    }
+
+    public function scheduledAppointments()
+    {
+        return $this->appointments()->where('state', '=', 'scheduled')->get();
+    }
+
+    public function ongoingAppointments()
+    {
+        return $this->appointments()->where('state', '=', 'ongoing')->get();
+    }
+
+    public function completedAppointments()
+    {
+        return $this->appointments()->where('state', '=', 'complete')->get();
     }
 }
