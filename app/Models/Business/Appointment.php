80ac3a6de44743d50fc2305283b022937fed41ce
Paulo Amosse Cuambe <37635729+paulocuambe@users.noreply.github.com>
2020-05-04 10:45:31 +0200

    adding appointment relationship with Doctor, Patient and Specilty models

--- app/Models/Business/Appointment.php
+++ app/Models/Business/Appointment.php
@@ -2,9 +2,29 @@
 
 namespace App\Models\Business;
 
+use App\Models\Authentication\Doctor;
+use App\Models\Authentication\Patient;
+use App\Models\Specialty;
 use Illuminate\Database\Eloquent\Model;
 
 class Appointment extends Model
 {
-    //
+    protected $guarded = [];
+
+    public function patient()
+    {
+        return $this->belongsTo(Patient::class);
+    }
+
+    public function doctor()
+    {
+        return $this->belongsTo(Doctor::class);
+    }
+
+    public function specialty()
+    {
+        return $this->belongsTo(Specialty::class);
+    }
+
+
 }
