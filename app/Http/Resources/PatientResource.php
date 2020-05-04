7a003c2f854cbb33896d343bf724ebcd549184e2
Paulo Amosse Cuambe <37635729+paulocuambe@users.noreply.github.com>
2020-05-04 10:05:50 +0200

    refactor to php style guide

--- app/Http/Resources/PatientResource.php
+++ app/Http/Resources/PatientResource.php
@@ -9,17 +9,17 @@ class PatientResource extends JsonResource
     /**
      * Transform the resource into an array.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return array
      */
     public function toArray($request)
     {
         return [
-            'id'=>$this->id,
-            'patient_code'=>$this->patient_code,
-            'job_title'=>$this->job_title,
-            'created_at'=>$this->created_at,
-            'updated_at'=>$this->created_at,
+            'id' => $this->id,
+            'patient_code' => $this->patient_code,
+            'job_title' => $this->job_title,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->created_at,
         ];
     }
 }
