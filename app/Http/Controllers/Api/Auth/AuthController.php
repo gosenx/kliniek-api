af53e7776a2b16813586703fde69997257d0085a
Paulo Amosse Cuambe <37635729+paulocuambe@users.noreply.github.com>
2020-05-19 22:14:13 +0200

    adding signup functionality to the API

---
+++ app/Http/Controllers/Api/Auth/AuthController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\Api\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreUser;
+use App\Http\Resources\UserResource;
+use App\Models\Authentication\Patient;
+use App\Models\Authentication\User;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Route;
+use Illuminate\Support\Facades\Hash;
+use Laravel\Passport\Client;
+
+class AuthController extends Controller
+{
+    //
+    public function register(StoreUser $request)
+    {
+        $client = Client::query()->where('id', $request->input('client_id'))->where('secret', $request->input('client_secret'))->first();
+
+        if (is_null($client)) {
+            return response()->json([
+                "message" => "Failed to authenticate the request",
+                "errors" => ["credentials" => "your client credentials are incorrect."],
+            ], 400);
+        }
+
+        $patient = new Patient();
+        $patient->job_title = $request->input('job_title');
+        $patient->save();
+
+        $user_data = $request->except(['job_title', 'client_id', 'client_secret']);
+        $data["password"] = Hash::make($request->input("password"));
+        $patient->user()->create($user_data);
+
+        return ['access_token' => $patient->user->createToken($patient->user->fullname)->accessToken];
+    }
+}
